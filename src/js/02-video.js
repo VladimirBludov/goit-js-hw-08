42cfb93bcbdfbced6a4bae9d5c19d54b39b48d78
Vladimir Bludov <vladimir.bloodov@gmail.com>
2021-12-23 11:19:37 +0200

    second task done

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,12 +1,17 @@
 import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
 const playerEl = document.querySelector('#vimeo-player');
 const player = new Player(playerEl);
 
-player.on('play', function () {
-  console.log('played the video!');
-});
+const savedTime = localStorage.getItem('videoplayer-current-time')
+  ? JSON.parse(localStorage.getItem('videoplayer-current-time'))
+  : 0;
 
-player.getVideoTitle().then(function (title) {
-  console.log('title:', title);
-});
+player.setCurrentTime(savedTime);
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(evt) {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(evt.seconds));
+}
